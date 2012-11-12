f5a1e7e33acd40d30079cd2806a2afe67cadff89
Jakub Krčma <jkrcma@gmail.com>
2012-11-12 11:36:39 +0100

    Changed broken custom migration name to description and added to status command output

--- lib/Doctrine/DBAL/Migrations/Tools/Console/Command/StatusCommand.php
+++ lib/Doctrine/DBAL/Migrations/Tools/Console/Command/StatusCommand.php
@@ -111,7 +111,11 @@ EOT
                 foreach ($migrations as $version) {
                     $isMigrated = in_array($version->getVersion(), $migratedVersions);
                     $status = $isMigrated ? '<info>migrated</info>' : '<error>not migrated</error>';
-                    $output->writeln('    <comment>>></comment> ' . $configuration->formatVersion($version->getVersion()) . ' (<comment>' . $version->getVersion() . '</comment>)' . str_repeat(' ', 30 - strlen($name)) . $status);
+                    $migrationName = $version->getMigration()->getDescription();
+                    if ($migrationName) {
+                        $migrationName = str_repeat(' ', 10) . $migrationName;
+                    }
+                    $output->writeln('    <comment>>></comment> ' . $configuration->formatVersion($version->getVersion()) . ' (<comment>' . $version->getVersion() . '</comment>)' . str_repeat(' ', 30 - strlen($name)) . $status . $migrationName);
                 }
             }
 
