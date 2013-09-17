e769dfcb3e38d760d99b0a1b740ff1de8c77e957
jeroenthora <jeroen.thora@cognosis.be>
2013-09-17 07:36:33 +0200

    fixed phpcs issues in migration files

--- lib/Doctrine/DBAL/Migrations/Tools/Console/Command/GenerateCommand.php
+++ lib/Doctrine/DBAL/Migrations/Tools/Console/Command/GenerateCommand.php
@@ -51,12 +51,18 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
  */
 class Version<version> extends AbstractMigration
 {
+    /**
+     * @param Schema $schema
+     */
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
 <up>
     }
 
+    /**
+     * @param Schema $schema
+     */
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
