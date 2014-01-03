7d3cde640f31df141d61d5d1be1b43d99315c7b8
Zsolt Petrik <petrik.zsolt@novusoftware.hu>
2014-01-03 11:47:09 +0100

    Renamed MappingTypes to TypeMappings, added config test and XML configuration for this

--- tests/Doctrine/DBAL/Migrations/Tests/Configuration/AbstractConfigurationTest.php
+++ tests/Doctrine/DBAL/Migrations/Tests/Configuration/AbstractConfigurationTest.php
@@ -32,4 +32,13 @@ abstract class AbstractConfigurationTest extends \Doctrine\DBAL\Migrations\Tests
         $config = $this->loadConfiguration();
         $this->assertEquals('doctrine_migration_versions_test', $config->getMigrationsTableName());
     }
+
+    public function testTypeMappings()
+    {
+        $config = $this->loadConfiguration();
+        $typeMappings = $config->getTypeMappings();
+        $this->assertEquals(array('enum' => 'string'), $typeMappings);
+        $this->assertArrayHasKey('enum', $typeMappings);
+        $this->assertEquals('string', $typeMappings['enum']);
+    }
 }
