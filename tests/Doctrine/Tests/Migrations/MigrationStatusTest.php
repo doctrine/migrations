6cf43223eb7bd059127d10905b794c501ae2d668
Benjamin Eberlei <kontakt@beberlei.de>
2014-03-23 16:47:04 +0100

    Add test for MigrationStatus#needsRepair

---
+++ tests/Doctrine/Tests/Migrations/MigrationStatusTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Doctrine\Tests\Migrations;
+
+use Doctrine\Migrations\MigrationCollection;
+use Doctrine\Migrations\MigrationStatus;
+use Doctrine\Migrations\MigrationInfo;
+use Doctrine\Migrations\Version;
+
+class MigrationStatusTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_needs_repair_when_one_migration_not_executed_successfully()
+    {
+        $failedMigration = new MigrationInfo(new Version(1));
+        $failedMigration->setSuccess(false);
+        $executedMigrations = new MigrationCollection();
+        $executedMigrations->add($failedMigration);
+
+        $status = new MigrationStatus(
+            $executedMigrations,
+            new MigrationCollection(),
+            true
+        );
+
+        $this->assertTrue($status->needsRepair());
+    }
+}
