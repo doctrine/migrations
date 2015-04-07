efb77c10dfb75485998a843154bbc44d8124c4d1
mike <mike.simonson@gmail.com>
2015-04-07 12:30:24 +0200

    Adding a test case for the version number

---
+++ tests/Doctrine/DBAL/Migrations/Tests/Tools/MigrationsVersionTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Doctrine\DBAL\Migrations\Tests\Tools;
+
+use Doctrine\DBAL\Migrations\MigrationsVersion;
+
+class MigrationsVersionTest extends \PHPUnit_Framework_TestCase {
+
+    private $MigrationVersionClass = 'Doctrine\DBAL\Migrations\MigrationsVersion';
+
+    public function testVersionNumber() {
+        $version = new MigrationsVersion();
+        $class = new \ReflectionClass($this->MigrationVersionClass);
+        $property = $class->getProperty('version');
+        $property->setAccessible(true);
+
+        $this->assertRegExp('/^v[0-9]{1,}\.[0-9]{1,}\.[0-9]{1,}(-([a-z]){1,}[0-9]{1,}){0,1}$/', $property->getValue(new MigrationsVersion()));
+    }
+
+    public function testIsACustomPharBuild(){
+        $class = new \ReflectionClass($this->MigrationVersionClass);
+        $method = $class->getMethod('isACustomPharBuild');
+        $method->setAccessible(true);
+
+        $this->assertFalse($method->invokeArgs(new MigrationsVersion(), array('@git-version@')), 'This is not a custom phar build.');
+        $this->assertTrue($method->invokeArgs(new MigrationsVersion(), array('v1.0.0-alpha3-125435')), 'This has been replaced by box and is thus a phar build.');
+    }
+
+}
