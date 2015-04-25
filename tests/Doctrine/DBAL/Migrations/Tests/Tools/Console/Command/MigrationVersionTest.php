85592796d985c730facf16267515ad2ef33a68e3
Ilya Antipenko <aivus@aivus.name>
2015-04-25 21:07:33 +0300

    Improve tests. Add new tests for migrations:version

--- tests/Doctrine/DBAL/Migrations/Tests/Tools/Console/Command/MigrationVersionTest.php
+++ tests/Doctrine/DBAL/Migrations/Tests/Tools/Console/Command/MigrationVersionTest.php
@@ -8,35 +8,42 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class MigrationVersionTest extends MigrationTestCase
 {
-    /**
-     * Test "range" option on migrate only versions in interval.
-     */
-    public function testRangeOption()
+    private $command;
+
+    /** @var Configuration */
+    private $configuration;
+
+    public function setUp()
     {
-        $command = $this
+        $this->command = $this
             ->getMockBuilder('Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand')
             ->setConstructorArgs(array('migrations:version'))
             ->setMethods(array('getMigrationConfiguration'))
             ->getMock();
 
-        /** @var Configuration $configuration */
-        $configuration = new Configuration($this->getSqliteConnection());
+        $this->configuration = new Configuration($this->getSqliteConnection());
+        $this->configuration->setMigrationsNamespace('DoctrineMigrations');
+        $this->configuration->setMigrationsDirectory(sys_get_temp_dir());
 
-        $configuration->setMigrationsNamespace('DoctrineMigrations');
-        $configuration->setMigrationsDirectory(sys_get_temp_dir());
-
-        $command
+        $this->command
             ->expects($this->once())
             ->method('getMigrationConfiguration')
-            ->will($this->returnValue($configuration));
+            ->will($this->returnValue($this->configuration));
 
-        $configuration->registerMigration(1233, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
-        $configuration->registerMigration(1234, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
-        $configuration->registerMigration(1235, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
-        $configuration->registerMigration(1239, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
-        $configuration->registerMigration(1240, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+    }
 
-        $commandTester = new CommandTester($command);
+    /**
+     * Test "--add --range" options on migrate only versions in interval.
+     */
+    public function testAddRangeOption()
+    {
+        $this->configuration->registerMigration(1233, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1234, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1235, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1239, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1240, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+
+        $commandTester = new CommandTester($this->command);
         $commandTester->execute(
             array(
                 '--add'       => true,
@@ -49,10 +56,108 @@ class MigrationVersionTest extends MigrationTestCase
             )
         );
 
-        $this->assertFalse($configuration->getVersion('1233')->isMigrated());
-        $this->assertTrue($configuration->getVersion('1234')->isMigrated());
-        $this->assertTrue($configuration->getVersion('1235')->isMigrated());
-        $this->assertTrue($configuration->getVersion('1239')->isMigrated());
-        $this->assertFalse($configuration->getVersion('1240')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1233')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1234')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1235')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1239')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1240')->isMigrated());
+    }
+
+    /**
+     * Test "--delete --range" options on migrate down only versions in interval.
+     */
+    public function testDeleteRangeOption()
+    {
+        $this->configuration->registerMigration(1233, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1234, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1235, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1239, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1240, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+
+        $this->configuration->getVersion('1233')->markMigrated();
+        $this->configuration->getVersion('1234')->markMigrated();
+        $this->configuration->getVersion('1239')->markMigrated();
+        $this->configuration->getVersion('1240')->markMigrated();
+
+
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute(
+            array(
+                '--delete'    => true,
+                '--range'     => true,
+                'version'     => '1234',
+                'version_end' => '1239',
+            ),
+            array(
+                'interactive' => false,
+            )
+        );
+
+        $this->assertTrue($this->configuration->getVersion('1233')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1234')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1235')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1239')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1240')->isMigrated());
+    }
+
+    /**
+     * Test "--add --all options on migrate all versions.
+     */
+    public function testAddAllOption()
+    {
+        $this->configuration->registerMigration(1233, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1234, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1235, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1239, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1240, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute(
+            array(
+                '--add'       => true,
+                '--all'       => true,
+            ),
+            array(
+                'interactive' => false,
+            )
+        );
+
+        $this->assertTrue($this->configuration->getVersion('1233')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1234')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1235')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1239')->isMigrated());
+        $this->assertTrue($this->configuration->getVersion('1240')->isMigrated());
+    }
+
+    /**
+     * Test "--delete --all" options on migrate down all versions.
+     */
+    public function testDeleteAllOption()
+    {
+        $this->configuration->registerMigration(1233, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1234, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1235, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1239, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+        $this->configuration->registerMigration(1240, 'Doctrine\DBAL\Migrations\Tests\ConfigMigration');
+
+        $this->configuration->getVersion('1233')->markMigrated();
+        $this->configuration->getVersion('1234')->markMigrated();
+
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute(
+            array(
+                '--delete'    => true,
+                '--all'       => true,
+            ),
+            array(
+                'interactive' => false,
+            )
+        );
+
+        $this->assertFalse($this->configuration->getVersion('1233')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1234')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1235')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1239')->isMigrated());
+        $this->assertFalse($this->configuration->getVersion('1240')->isMigrated());
     }
 }
