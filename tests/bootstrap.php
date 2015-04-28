1b33c50353b37d6c7c9df1093994dafa271be7f4
Christopher Davis <cdavis9999@gmail.com>
2015-04-28 11:58:24 -0400

    Use a Local Copy of PHPUnit to Prevent HHVM Errors
    See https://github.com/facebook/hhvm/issues/5215
    
    Also adjust a `require_once` call in `tests/bootstrap.php` to make sure
    the loader is always available.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Doctrine\DBAL\Migrations\Tests', __DIR__);
