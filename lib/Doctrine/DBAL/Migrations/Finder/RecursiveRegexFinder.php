a60d5fdf084a6225c3e0aa3723320519fac82746
Christopher Davis <cdavis9999@gmail.com>
2015-05-02 11:41:01 -0700

    Introduce RecursiveRegexFinder
    Searches through a directory recursively for migration files.

---
+++ lib/Doctrine/DBAL/Migrations/Finder/RecursiveRegexFinder.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\DBAL\Migrations\Finder;
+
+/**
+ * A MigrationFinder implementation that uses a RegexIterator along with a
+ * RecursiveDirectoryIterator.
+ *
+ * @since   1.0.0-alpha3
+ */
+final class RecursiveRegexFinder extends AbstractFinder
+{
+    const DEFAULT_PATTERN = '/Version\d{14}\.php$/i';
+
+    /**
+     * The regex pattern to match against.
+     *
+     * @var     string
+     */
+    private $pattern;
+
+    public function __construct($pattern=null)
+    {
+        $this->pattern = $pattern ?: self::DEFAULT_PATTERN;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findMigrations($directory, $namespace=null)
+    {
+        $dir = realpath($directory);
+        if (false === $dir || !is_dir($dir)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Cannot load migrations from "%s" because it is not a valid directory',
+                $directory
+            ));
+        }
+
+        $migrations = array();
+        foreach ($this->createIterator($dir) as $file) {
+            static::requireOnce($file);
+            $className = $file->getBasename('.php');
+            $version = substr($className, -14);
+            $migrations[$version] = sprintf('%s\\%s', $namespace, $className);
+        }
+
+        return $migrations;
+    }
+
+    private function createIterator($dir)
+    {
+        return new \RegexIterator(
+            new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS),
+                \RecursiveIteratorIterator::LEAVES_ONLY
+            ),
+            $this->pattern
+        );
+    }
+}
---
+++ tests/Doctrine/DBAL/Migrations/Tests/Finder/RecursiveRegexFinderTest.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\DBAL\Migrations\Tests\Finder;
+
+use Doctrine\DBAL\Migrations\Finder\RecursiveRegexFinder;
+use Doctrine\DBAL\Migrations\Tests\MigrationTestCase;
+
+class RecursiveRegexFinderTest extends MigrationTestCase
+{
+    private $finder;
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testBadFilenameCausesErrorWhenFindingMigrations()
+    {
+        $this->finder->findMigrations(__DIR__.'/does/not/exist/at/all');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testNonDirectoryCausesErrorWhenFindingMigrations()
+    {
+        $this->finder->findMigrations(__FILE__);
+    }
+
+    public function testFindMigrationsReturnsTheExpectedFilesFromDirectory()
+    {
+        $migrations = $this->finder->findMigrations(__DIR__.'/_files', 'TestMigrations');
+
+        $this->assertCount(3, $migrations);
+        $this->assertArrayHasKey('20150502000000', $migrations);
+        $this->assertEquals('TestMigrations\\Version20150502000000', $migrations['20150502000000']);
+        $this->assertArrayHasKey('20150502000001', $migrations);
+        $this->assertEquals('TestMigrations\\Version20150502000001', $migrations['20150502000001']);
+        $this->assertArrayHasKey('20150502000002', $migrations);
+        $this->assertEquals('TestMigrations\\CustomVersion20150502000002', $migrations['20150502000002']);
+    }
+
+    protected function setUp()
+    {
+        $this->finder = new RecursiveRegexFinder();
+    }
+}
