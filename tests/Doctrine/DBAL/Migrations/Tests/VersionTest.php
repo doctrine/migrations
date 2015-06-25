e737f77053af7b02c5bc515c7bc541444792281a
Martin Westra <github@diewom.com>
2015-06-25 21:35:45 +0200

    - added tests for Version

--- tests/Doctrine/DBAL/Migrations/Tests/VersionTest.php
+++ tests/Doctrine/DBAL/Migrations/Tests/VersionTest.php
@@ -33,6 +33,97 @@ class VersionTest extends MigrationTestCase
         $this->assertEquals($versionName, $version->getVersion());
         $this->assertEquals($versionDescription, $version->getMigration()->getDescription());
     }
+
+    /**
+     * Test outputQueryTime on all queries
+     */
+    public function testOutputQueryTimeAllQueries()
+    {
+        $outputWriterMock = $this->getMock('Doctrine\DBAL\Migrations\OutputWriter');
+        $outputWriterMock->expects($this->once())->method('write');
+        $configuration = new Configuration($this->getSqliteConnection(), $outputWriterMock);
+        $reflectionVersion = new \ReflectionClass('Doctrine\DBAL\Migrations\Version');
+        $method = $reflectionVersion->getMethod('outputQueryTime');
+        $method->setAccessible(true);
+
+        $version = new Version(
+            $configuration,
+            $versionName = '003',
+            'Doctrine\DBAL\Migrations\Tests\VersionTest_Migration'
+        );
+        $this->assertNull($method->invoke($version, 0, true));
+    }
+
+    /**
+     * Test outputQueryTime not on all queries
+     */
+    public function testOutputQueryTimeNotAllQueries()
+    {
+        $outputWriterMock = $this->getMock('Doctrine\DBAL\Migrations\OutputWriter');
+        $outputWriterMock->expects($this->exactly(0))->method('write');
+        $configuration = new Configuration($this->getSqliteConnection(), $outputWriterMock);
+        $reflectionVersion = new \ReflectionClass('Doctrine\DBAL\Migrations\Version');
+        $method = $reflectionVersion->getMethod('outputQueryTime');
+        $method->setAccessible(true);
+
+        $version = new Version(
+            $configuration,
+            $versionName = '003',
+            'Doctrine\DBAL\Migrations\Tests\VersionTest_Migration'
+        );
+        $this->assertNull($method->invoke($version, 0, false));
+    }
+
+    /**
+     * Test outputQueryTime not on all queries
+     * @dataProvider stateProvider
+     */
+    public function testGetExecutionState($state)
+    {
+        $configuration = new Configuration($this->getSqliteConnection());
+        $version = new Version(
+            $configuration,
+            $versionName = '003',
+            'Doctrine\DBAL\Migrations\Tests\VersionTest_Migration'
+        );
+        $reflectionVersion = new \ReflectionClass('Doctrine\DBAL\Migrations\Version');
+        $stateProperty = $reflectionVersion->getProperty('state');
+        $stateProperty->setAccessible(true);
+        $stateProperty->setValue($version, $state);
+        $this->assertNotEmpty($version->getExecutionState());
+    }
+
+    /**
+     * Provides states
+     * @return array
+     */
+    public function stateProvider()
+    {
+        return array(
+            array(Version::STATE_NONE),
+            array(Version::STATE_EXEC),
+            array(Version::STATE_POST),
+            array(Version::STATE_PRE),
+            array(-1),
+        );
+    }
+
+    /**
+     * Test add sql
+     */
+    public function testAddSql()
+    {
+        $configuration = new Configuration($this->getSqliteConnection());
+        $version = new Version(
+            $configuration,
+            $versionName = '003',
+            'Doctrine\DBAL\Migrations\Tests\VersionTest_Migration'
+        );
+        $this->assertNull($version->addSql('SELECT * FROM foo'));
+        $this->assertNull($version->addSql(array('SELECT * FROM foo')));
+        $this->assertNull($version->addSql(array('SELECT * FROM foo WHERE id = ?'), array(1)));
+        $this->assertNull($version->addSql(array('SELECT * FROM foo WHERE id = ?'), array(1), array(\PDO::PARAM_INT)));
+    }
 }
 
 /**
