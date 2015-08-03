8b44960e0a55fcbaa94076275603bf8f47abe608
davidhavl <info@davidhavl.com>
2015-08-03 13:03:16 +0100

    Added migrations key to test config to increased code coverage

--- tests/Doctrine/DBAL/Migrations/Tests/Configuration/_files/config.php
+++ tests/Doctrine/DBAL/Migrations/Tests/Configuration/_files/config.php
@@ -3,5 +3,6 @@ return array(
 'name'                 => 'Doctrine Sandbox Migrations',
 'migrations_namespace' => 'DoctrineMigrationsTest',
 'table_name'           => 'doctrine_migration_versions_test',
-'migrations_directory' => '.'
+'migrations_directory' => '.',
+'migrations'           => array()
 );
