df29f93518a976f988026a496b35f5bbf5768fd7
Andreas Möller <am@localheinz.com>
2015-08-18 14:00:29 +0200

    Enhancement: Assert that OutputWriter can be injected

---
+++ tests/Doctrine/DBAL/Migrations/Tests/Configuration/ConfigurationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Doctrine\DBAL\Migrations\Tests\Configuration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\DBAL\Migrations\OutputWriter;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructorSetsOutputWriter()
+    {
+        $outputWriter = $this->getOutputWriterMock();
+
+        $configuration = new Configuration(
+            $this->getConnectionMock(),
+            $outputWriter
+        );
+
+        $this->assertSame($outputWriter, $configuration->getOutputWriter());
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Connection
+     */
+    private function getConnectionMock()
+    {
+        return $this->getMockBuilder('Doctrine\DBAL\Connection')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|OutputWriter
+     */
+    private function getOutputWriterMock()
+    {
+        return $this->getMockBuilder('Doctrine\DBAL\Migrations\OutputWriter')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+}
