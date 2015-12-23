bffd726e2193c04f88a23154deb1c618a966996d
mike <mike.simonson@gmail.com>
2015-12-23 18:08:58 +0100

    Tag the v1.2.1 as a bug fix for the write-sql functionnality

--- lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
+++ lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
@@ -21,7 +21,7 @@ namespace Doctrine\DBAL\Migrations;
 
 class MigrationsVersion
 {
-    private static $version = 'v1.2.0';
+    private static $version = 'v1.2.1';
 
     public static function VERSION() {
         $gitversion = '@git-version@';
