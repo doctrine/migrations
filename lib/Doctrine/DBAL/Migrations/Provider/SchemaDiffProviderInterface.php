614ea6f0d3a8e5f0669d10b018cf0b2294ea417c
mike <mike.simonson@gmail.com>
2016-01-05 01:29:13 +0100

    And adding the missing interface

---
+++ lib/Doctrine/DBAL/Migrations/Provider/SchemaDiffProviderInterface.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Doctrine\DBAL\Migrations\Provider;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Generates `Schema` objects to be passed to the migrations class.
+ *
+ * @since   1.3
+ */
+interface SchemaDiffProviderInterface
+{
+    /**
+     * Create the schema that represent the current state of the database.
+     *
+     * @return Schema
+     */
+    public function createFromSchema();
+
+    /**
+     * Create the schema that will represent the future state of the database
+     *
+     * @param Schema $fromSchema
+     * @return Schema
+     */
+    public function createToSchema(Schema $fromSchema);
+
+    /**
+     * Return an array of sql statement that migrate the database state from the
+     * fromSchema to the toSchema.
+     *
+     * @param Schema $fromSchema
+     * @param Schema $toSchema
+     *
+     * @return array
+     */
+    public function getSqlDiffToMigrate(Schema $fromSchema,Schema $toSchema);
+}
