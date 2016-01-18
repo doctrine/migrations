1979fc286f6aa46541a2a5d6b599a0345d8967e2
mike <mike.simonson@gmail.com>
2016-01-18 22:18:01 +0100

    Adding a regression test for the configuration

--- tests/Doctrine/DBAL/Migrations/Tests/Configuration/ConfigurationTest.php
+++ tests/Doctrine/DBAL/Migrations/Tests/Configuration/ConfigurationTest.php
@@ -4,9 +4,18 @@ namespace Doctrine\DBAL\Migrations\Tests\Configuration;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\DBAL\Migrations\Migration;
+use Doctrine\DBAL\Migrations\MigrationException;
 use Doctrine\DBAL\Migrations\OutputWriter;
+use Doctrine\DBAL\Migrations\Tests\MigrationTestCase;
+use Doctrine\DBAL\Migrations\Tests\Stub\Configuration\AutoloadVersions\Version1Test;
+use Doctrine\DBAL\Migrations\Tests\Stub\Configuration\AutoloadVersions\Version2Test;
+use Doctrine\DBAL\Migrations\Tests\Stub\Configuration\AutoloadVersions\Version3Test;
+use Doctrine\DBAL\Migrations\Tests\Stub\Configuration\AutoloadVersions\Version4Test;
+use Doctrine\DBAL\Migrations\Tests\Stub\Configuration\AutoloadVersions\Version5Test;
+use Doctrine\DBAL\Migrations\Version;
 
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends MigrationTestCase
 {
     public function testConstructorSetsOutputWriter()
     {
@@ -81,5 +90,111 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
 
         $configuration->setMigrationsColumnName('foobar');
         $this->assertSame('foobar', $configuration->getMigrationsColumnName());
+    }
+
+    /**
+     * @dataProvider methodsThatNeedsVersionsLoaded
+     */
+    public function testVersionsTryToGetLoadedIfNotAlreadyLoadedWhenAccessingMethodThatNeedThemEvenIfNoNamespaceSet($method, $args)
+    {
+        $this->setExpectedException(MigrationException::class, 'Migrations namespace must be configured in order to use Doctrine migrations.');
+
+        $configuration = new Configuration($this->getConnectionMock());
+        call_user_func_array([$configuration, $method], $args);
+    }
+
+    /**
+     * @dataProvider methodsThatNeedsVersionsLoaded
+     */
+    public function testVersionsTryToGetLoadedIfNotAlreadyLoadedWhenAccessingMethodThatNeedThemEvenIfNoDirectorySet($method, $args)
+    {
+        $this->setExpectedException(MigrationException::class, 'Migrations directory must be configured in order to use Doctrine migrations.');
+
+        $configuration = new Configuration($this->getConnectionMock());
+        $configuration->setMigrationsNamespace('DoctrineMigrations\\');
+
+        call_user_func_array([$configuration, $method], $args);
+    }
+
+    /**
+     * @dataProvider methodsThatNeedsVersionsLoaded
+     */
+    public function testVersionsTryToGetLoadedIfNotAlreadyLoadedWhenAccessingMethodThatNeedThem($method, $args, $expectedResult)
+    {
+        $configuration = new Configuration($this->getSqliteConnection());
+        $configuration->setMigrationsNamespace(str_replace('\Version1Test', '', Version1Test::class));
+        $configuration->setMigrationsDirectory(__DIR__ . '/../Stub/Configuration/AutoloadVersions');
+
+        $result = call_user_func_array([$configuration, $method], $args);
+        if ($method == 'getMigrationsToExecute') {
+            $result = array_keys($result);
+        }
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * @dataProvider methodsThatNeedsVersionsLoadedWithAlreadyMigratedMigrations
+     */
+    public function testVersionsTryToGetLoadedIfNotAlreadyLoadedWhenAccessingMethodThatNeedThemEvenIfSomeMigrationsAreAlreadyMigrated($method, $args, $expectedResult)
+    {
+        $configuration = new Configuration($this->getSqliteConnection());
+        $configuration->setMigrationsNamespace(str_replace('\Version1Test', '', Version1Test::class));
+        $configuration->setMigrationsDirectory(__DIR__ . '/../Stub/Configuration/AutoloadVersions');
+        $migration = new Migration($configuration);
+        $migration->migrate('3Test');
+
+        $result = call_user_func_array([$configuration, $method], $args);
+        if ($method == 'getMigrationsToExecute') {
+            $result = array_keys($result);
+        }
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function methodsThatNeedsVersionsLoadedWithAlreadyMigratedMigrations()
+    {
+        return [
+            ['hasVersion', ['4Test'], true],
+            ['getAvailableVersions', [], ['1Test', '2Test', '3Test', '4Test', '5Test']],
+            ['getCurrentVersion', [], '3Test'],
+            ['getRelativeVersion', ['3Test', -1], '2Test'],
+            ['getNumberOfAvailableMigrations', [], 5],
+            ['getLatestVersion', [], '5Test'],
+            ['getMigrationsToExecute', ['up', 5], [
+                '4Test',
+                '5Test',
+            ]],
+            ['getMigrationsToExecute', ['up', 4], [
+                '4Test',
+            ]],
+            ['getMigrationsToExecute', ['down', 0], [
+                '3Test',
+                '2Test',
+                '1Test',
+            ]],
+            ['getMigrationsToExecute', ['down', 2], [
+                '3Test',
+            ]],
+        ];
+    }
+
+    public function methodsThatNeedsVersionsLoaded()
+    {
+        return [
+            ['hasVersion', ['3Test'], true],
+            ['getAvailableVersions', [], ['1Test', '2Test', '3Test', '4Test', '5Test']],
+            ['getCurrentVersion', [], '0'],
+            ['getRelativeVersion', ['3Test', -1], '2Test'],
+            ['getNumberOfAvailableMigrations', [], 5],
+            ['getLatestVersion', [], '5Test'],
+            ['getMigrationsToExecute', ['up', 5], [
+                '1Test',
+                '2Test',
+                '3Test',
+                '4Test',
+                '5Test',
+            ]],
+            ['getMigrationsToExecute', ['down', 0], []],
+            ['getMigrationsToExecute', ['down', 2], []],
+        ];
     }
 }
