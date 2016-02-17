11ecccb4568d9f0718fc9e4c94963e15c2803651
mike <mike.simonson@gmail.com>
2016-02-17 12:36:38 +0100

    Fixing an issue in the order at which some configuration key are loaded
    There is validation that check that the namespace and folder name are
    set. Namely the validate method in the configuration class.
    
    Because of that check we need to make sure that the namespace is set
    before any migration are loaded, as the check happen there.

---
+++ tests/Doctrine/DBAL/Migrations/Tests/Configuration/_files/config_order_1.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+'name'                 => 'Doctrine Sandbox Migrations',
+'migrations_namespace' => 'DoctrineMigrationsTest',
+'table_name'           => 'doctrine_migration_versions_test',
+'migrations_directory' => '.',
+'migrations'           => array()
+);
