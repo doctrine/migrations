c81147c0f2938a6566594455367e095150547f72
mike <mike.simonson@gmail.com>
2016-12-25 23:47:24 +0100

    preparing the release of the v1.5.0

--- lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
+++ lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
@@ -21,7 +21,7 @@ namespace Doctrine\DBAL\Migrations;
 
 class MigrationsVersion
 {
-    private static $version = 'v1.4.0';
+    private static $version = 'v1.5.0';
 
     public static function VERSION() {
         $gitversion = '@git-version@';
