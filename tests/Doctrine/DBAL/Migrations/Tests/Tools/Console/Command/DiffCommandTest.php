59a53d0dd04f21dae09e8fc34381d3b91d6ac087
Andreas Braun <git@alcaeus.org>
2017-11-08 15:50:54 +0100

    Enable custom template for diff command as well

--- tests/Doctrine/DBAL/Migrations/Tests/Tools/Console/Command/DiffCommandTest.php
+++ tests/Doctrine/DBAL/Migrations/Tests/Tools/Console/Command/DiffCommandTest.php
@@ -10,7 +10,9 @@ use Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand;
 
 class DiffCommandTest extends CommandTestCase
 {
-    const VERSION = '20160705000000';
+    const VERSION                       = '20160705000000';
+    const CUSTOM_RELATIVE_TEMPLATE_NAME = 'tests/Doctrine/DBAL/Migrations/Tests/Tools/Console/Command/_files/migration.tpl';
+    const CUSTOM_ABSOLUTE_TEMPLATE_NAME = __DIR__ . '/_files/migration.tpl';
 
     private $root;
     private $migrationFile;
@@ -29,6 +31,38 @@ class DiffCommandTest extends CommandTestCase
         $content = $this->root->getChild($this->migrationFile)->getContent();
         self::assertContains('class Version' . self::VERSION, $content);
         self::assertContains('CREATE TABLE example', $content);
+    }
+
+    public static function provideCustomTemplateNames() : array
+    {
+        return [
+            'relativePath' => [self::CUSTOM_RELATIVE_TEMPLATE_NAME],
+            'absolutePath' => [self::CUSTOM_ABSOLUTE_TEMPLATE_NAME],
+        ];
+    }
+
+    /**
+     * @dataProvider provideCustomTemplateNames
+     */
+    public function testCommandCreatesNewMigrationsFileWithAVersionAndACustomTemplateFromConfiguration(string $templateName)
+    {
+        $this->config->expects($this->once())
+            ->method('generateVersionNumber')
+            ->willReturn(self::VERSION);
+
+        $this->config->expects($this->once())
+            ->method('getCustomTemplate')
+            ->willReturn($templateName);
+
+        list($tester, $statusCode) = $this->executeCommand([]);
+
+        self::assertSame(0, $statusCode);
+        self::assertContains($this->migrationFile, $tester->getDisplay());
+        self::assertTrue($this->root->hasChild($this->migrationFile));
+        $content = $this->root->getChild($this->migrationFile)->getContent();
+        self::assertContains('class Version' . self::VERSION, $content);
+        self::assertContains('CREATE TABLE example', $content);
+        self::assertContains('public function customTemplate()', $content);
     }
 
     protected function setUp()
