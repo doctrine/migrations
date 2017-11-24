e3faf7c96b8a6084045dedcaf51f74c7834644d4
mike <mike.simonson@gmail.com>
2017-11-24 15:13:17 +0100

    Update the version number of doctrine migrations

--- lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
+++ lib/Doctrine/DBAL/Migrations/MigrationsVersion.php
@@ -4,7 +4,7 @@ namespace Doctrine\DBAL\Migrations;
 
 class MigrationsVersion
 {
-    private static $version = 'v1.5.0';
+    private static $version = 'v1.6.2';
 
     public static function VERSION()
     {
