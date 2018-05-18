49711e8f73850497722ab2eff90cf615bd6348c9
Jonathan H. Wage <jonwage@gmail.com>
2018-05-18 20:52:53 +0100

    Upgrade to Box 3 for phar generation.

--- build-phar.sh
+++ build-phar.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-git fetch origin master --tags
-
-if [ -f composer.lock ]; then
-  rm composer.lock
-fi
-
-composer install --no-dev --optimize-autoloader
-
-mkdir -p build
-
-if [ ! -f box.phar ]; then
-    wget https://github.com/box-project/box2/releases/download/2.6.0/box-2.6.0.phar -O box.phar
-fi
-
-php box.phar build -vv
+php box.phar compile -vv
