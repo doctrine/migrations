f72ad747ff88732a41d7e3782ca8696bbe4c663e
Christopher Davis <cdavis9999@gmail.com>
2018-05-19 06:36:28 -0500

    Put Actual Migration Classes in the Finder Test Files

---
+++ tests/Doctrine/Migrations/Tests/Finder/_files/deep/Version1_reset_versions.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace TestMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version1_reset_versions extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // ignored
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // ignored
+    }
+}
