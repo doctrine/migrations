fed3f67c61792c0d6de4673d44124f1e0b3ac553
Michael Moravec <me@majkl.me>
2018-06-04 15:19:30 +0200

    Refresh bin/ entrypoints

--- bin/doctrine-migrations.php
+++ bin/doctrine-migrations.php
@@ -2,72 +2,88 @@
 
 declare(strict_types=1);
 
+namespace Doctrine\Migrations;
+
 use Doctrine\Migrations\Tools\Console\ConsoleRunner;
-use Symfony\Component\Console\Helper\DialogHelper;
+use Phar;
 use Symfony\Component\Console\Helper\HelperSet;
 use Symfony\Component\Console\Helper\QuestionHelper;
+use const DIRECTORY_SEPARATOR;
+use const E_USER_ERROR;
+use const PHP_EOL;
+use function extension_loaded;
+use function file_exists;
+use function getcwd;
+use function is_readable;
+use function trigger_error;
 
 $autoloadFiles = [
     __DIR__ . '/../vendor/autoload.php',
-    __DIR__ . '/../../../autoload.php'
+    __DIR__ . '/../../../autoload.php',
 ];
 
-$autoloader = false;
+$autoloaderFound = false;
 
 foreach ($autoloadFiles as $autoloadFile) {
-    if (file_exists($autoloadFile)) {
-        require_once $autoloadFile;
-        $autoloader = true;
+    if (! file_exists($autoloadFile)) {
+        continue;
     }
+
+    require_once $autoloadFile;
+    $autoloaderFound = true;
 }
 
-if (!$autoloader) {
-    if (extension_loaded('phar') && ($uri = Phar::running())) {
-        echo 'The phar has been built without dependencies' . PHP_EOL;
+if (! $autoloaderFound) {
+    if (extension_loaded('phar') && Phar::running() !== '') {
+        echo 'The PHAR was built without dependencies!' . PHP_EOL;
+        exit(1);
     }
 
-    die('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+    echo 'vendor/autoload.php could not be found. Did you run `composer install`?', PHP_EOL;
+    exit(1);
 }
 
 // Support for using the Doctrine ORM convention of providing a `cli-config.php` file.
-$directories = [getcwd(), getcwd() . DIRECTORY_SEPARATOR . 'config'];
+$configurationDirectories = [
+    getcwd(),
+    getcwd() . DIRECTORY_SEPARATOR . 'config',
+];
 
-$configFile = null;
-foreach ($directories as $directory) {
-    $configFile = $directory . DIRECTORY_SEPARATOR . 'cli-config.php';
+$configurationFile = null;
+foreach ($configurationDirectories as $configurationDirectory) {
+    $configurationFilePath = $configurationDirectory . DIRECTORY_SEPARATOR . 'cli-config.php';
 
-    if (file_exists($configFile)) {
-        break;
+    if (! file_exists($configurationFilePath)) {
+        continue;
     }
+
+    $configurationFile = $configurationFilePath;
+    break;
 }
 
 $helperSet = null;
-if (file_exists($configFile)) {
-    if ( ! is_readable($configFile)) {
-        trigger_error(
-            'Configuration file [' . $configFile . '] does not have read permission.', E_USER_ERROR
-        );
+if ($configurationFile !== null) {
+    if (! is_readable($configurationFile)) {
+        trigger_error('Configuration file [' . $configurationFile . '] does not have read permission.', E_USER_ERROR);
+        exit(1);
     }
 
-    $helperSet = require $configFile;
+    $helperSet = require $configurationFile;
 
-    if ( ! ($helperSet instanceof HelperSet)) {
+    if (! $helperSet instanceof HelperSet) {
         foreach ($GLOBALS as $helperSetCandidate) {
-            if ($helperSetCandidate instanceof HelperSet) {
-                $helperSet = $helperSetCandidate;
-                break;
+            if (! $helperSetCandidate instanceof HelperSet) {
+                continue;
             }
+
+            $helperSet = $helperSetCandidate;
+            break;
         }
     }
 }
 
-$helperSet = ($helperSet) ?: new HelperSet();
-
-if(class_exists('\Symfony\Component\Console\Helper\QuestionHelper')) {
-    $helperSet->set(new QuestionHelper(), 'question');
-} else {
-    $helperSet->set(new DialogHelper(), 'dialog');
-}
+$helperSet = $helperSet ?? new HelperSet();
+$helperSet->set(new QuestionHelper(), 'question');
 
 $commands = [];
 
