321bbe440e3df7b05ab33716124d5963d6506d47
Michael Moravec <me@majkl.me>
2018-06-04 15:20:59 +0200

    Run bin/ entrypoint in isolation (inside closure)

--- bin/doctrine-migrations.php
+++ bin/doctrine-migrations.php
@@ -17,74 +17,76 @@ use function getcwd;
 use function is_readable;
 use function trigger_error;
 
-$autoloadFiles = [
-    __DIR__ . '/../vendor/autoload.php',
-    __DIR__ . '/../../../autoload.php',
-];
+(function () : void {
+    $autoloadFiles = [
+        __DIR__ . '/../vendor/autoload.php',
+        __DIR__ . '/../../../autoload.php',
+    ];
 
-$autoloaderFound = false;
+    $autoloaderFound = false;
 
-foreach ($autoloadFiles as $autoloadFile) {
-    if (! file_exists($autoloadFile)) {
-        continue;
+    foreach ($autoloadFiles as $autoloadFile) {
+        if (! file_exists($autoloadFile)) {
+            continue;
+        }
+
+        require_once $autoloadFile;
+        $autoloaderFound = true;
     }
 
-    require_once $autoloadFile;
-    $autoloaderFound = true;
-}
+    if (! $autoloaderFound) {
+        if (extension_loaded('phar') && Phar::running() !== '') {
+            echo 'The PHAR was built without dependencies!' . PHP_EOL;
+            exit(1);
+        }
 
-if (! $autoloaderFound) {
-    if (extension_loaded('phar') && Phar::running() !== '') {
-        echo 'The PHAR was built without dependencies!' . PHP_EOL;
+        echo 'vendor/autoload.php could not be found. Did you run `composer install`?', PHP_EOL;
         exit(1);
     }
 
-    echo 'vendor/autoload.php could not be found. Did you run `composer install`?', PHP_EOL;
-    exit(1);
-}
+    // Support for using the Doctrine ORM convention of providing a `cli-config.php` file.
+    $configurationDirectories = [
+        getcwd(),
+        getcwd() . DIRECTORY_SEPARATOR . 'config',
+    ];
 
-// Support for using the Doctrine ORM convention of providing a `cli-config.php` file.
-$configurationDirectories = [
-    getcwd(),
-    getcwd() . DIRECTORY_SEPARATOR . 'config',
-];
+    $configurationFile = null;
+    foreach ($configurationDirectories as $configurationDirectory) {
+        $configurationFilePath = $configurationDirectory . DIRECTORY_SEPARATOR . 'cli-config.php';
 
-$configurationFile = null;
-foreach ($configurationDirectories as $configurationDirectory) {
-    $configurationFilePath = $configurationDirectory . DIRECTORY_SEPARATOR . 'cli-config.php';
+        if (! file_exists($configurationFilePath)) {
+            continue;
+        }
 
-    if (! file_exists($configurationFilePath)) {
-        continue;
+        $configurationFile = $configurationFilePath;
+        break;
     }
 
-    $configurationFile = $configurationFilePath;
-    break;
-}
+    $helperSet = null;
+    if ($configurationFile !== null) {
+        if (! is_readable($configurationFile)) {
+            trigger_error('Configuration file [' . $configurationFile . '] does not have read permission.', E_USER_ERROR);
+            exit(1);
+        }
 
-$helperSet = null;
-if ($configurationFile !== null) {
-    if (! is_readable($configurationFile)) {
-        trigger_error('Configuration file [' . $configurationFile . '] does not have read permission.', E_USER_ERROR);
-        exit(1);
-    }
+        $helperSet = require $configurationFile;
 
-    $helperSet = require $configurationFile;
+        if (! $helperSet instanceof HelperSet) {
+            foreach ($GLOBALS as $helperSetCandidate) {
+                if (! $helperSetCandidate instanceof HelperSet) {
+                    continue;
+                }
 
-    if (! $helperSet instanceof HelperSet) {
-        foreach ($GLOBALS as $helperSetCandidate) {
-            if (! $helperSetCandidate instanceof HelperSet) {
-                continue;
+                $helperSet = $helperSetCandidate;
+                break;
             }
-
-            $helperSet = $helperSetCandidate;
-            break;
         }
     }
-}
 
-$helperSet = $helperSet ?? new HelperSet();
-$helperSet->set(new QuestionHelper(), 'question');
+    $helperSet = $helperSet ?? new HelperSet();
+    $helperSet->set(new QuestionHelper(), 'question');
 
-$commands = [];
+    $commands = [];
 
-ConsoleRunner::run($helperSet, $commands);
+    ConsoleRunner::run($helperSet, $commands);
+})();
