78c86b7f69a544d54318916d4cc742b9824122a4
Pau <pau@pixelcons.com>
2018-09-27 09:18:42 +0200

    Remove unused class Version

--- lib/Doctrine/Migrations/AbstractMigration.php
+++ lib/Doctrine/Migrations/AbstractMigration.php
@@ -3,7 +3,6 @@
 namespace Doctrine\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration as BaseAbstractMigration;
-use Doctrine\DBAL\Migrations\Version;
 
 abstract class AbstractMigration extends BaseAbstractMigration
 {
