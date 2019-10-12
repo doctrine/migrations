f7735c2c94cc5d11e56fb8776b8765ca7990dae9
Asmir Mustafic <goetas@gmail.com>
2019-10-12 23:42:01 +0200

    rename UnableToLoadResource

--- lib/Doctrine/Migrations/Configuration/Exception/UnableToLoadResource.php
+++ lib/Doctrine/Migrations/Configuration/Exception/UnableToLoadResource.php
@@ -7,9 +7,9 @@ namespace Doctrine\Migrations\Configuration\Exception;
 use LogicException;
 use function sprintf;
 
-final class UnknownResource extends LogicException implements ConfigurationException
+final class UnableToLoadResource extends LogicException implements ConfigurationException
 {
-    public static function new(string $loader) : self
+    public static function with(string $loader) : self
     {
         return new self(sprintf('The provided resource can not be loaded by the loader "%s".', $loader), 10);
     }
