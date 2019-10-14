bc12a432ba93a59ad153673a5fc86cdda675dc93
Asmir Mustafic <goetas@gmail.com>
2019-10-14 11:00:41 +0200

    do not allow invalid config keys

--- tests/Doctrine/Migrations/Tests/Configuration/Loader/ArrayLoaderTest.php
+++ tests/Doctrine/Migrations/Tests/Configuration/Loader/ArrayLoaderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\Migrations\Tests\Configuration\Loader;
 
+use Doctrine\Migrations\Configuration\Exception\InvalidConfigurationKey;
 use Doctrine\Migrations\Configuration\Exception\UnableToLoadResource;
 use Doctrine\Migrations\Configuration\Loader\ArrayLoader;
 use PHPUnit\Framework\TestCase;
@@ -16,5 +17,25 @@ class ArrayLoaderTest extends TestCase
         $this->expectExceptionMessage('The provided resource can not be loaded by the loader "Doctrine\Migrations\Configuration\Loader\ArrayLoader".');
         $loader = new ArrayLoader();
         $loader->load(null);
+    }
+
+    public function testInvalidKey() : void
+    {
+        $this->expectException(InvalidConfigurationKey::class);
+        $this->expectExceptionMessage('Migrations configuration key "foo" does not exist');
+        $loader = new ArrayLoader();
+        $loader->load([
+            'foo' => 'aaa'
+        ]);
+    }
+
+    public function testInvalidKeyInteger() : void
+    {
+        $this->expectException(InvalidConfigurationKey::class);
+        $this->expectExceptionMessage('Migrations configuration key "0" does not exist.');
+        $loader = new ArrayLoader();
+        $loader->load([
+            'aaa'
+        ]);
     }
 }
