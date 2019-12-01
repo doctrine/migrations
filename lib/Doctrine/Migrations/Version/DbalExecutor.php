caaf1a58f08d795267274e3dfc644fe4157f3bc3
Asmir Mustafic <goetas@gmail.com>
2019-12-01 09:52:51 +0100

    use executeUpdate for migration queries as it is a write operation

--- lib/Doctrine/Migrations/Version/DbalExecutor.php
+++ lib/Doctrine/Migrations/Version/DbalExecutor.php
@@ -332,9 +332,9 @@ final class DbalExecutor implements Executor
             $this->outputSqlQuery($key, $query);
 
             if (! isset($this->params[$key])) {
-                $this->connection->executeQuery($query);
+                $this->connection->executeUpdate($query);
             } else {
-                $this->connection->executeQuery($query, $this->params[$key], $this->types[$key]);
+                $this->connection->executeUpdate($query, $this->params[$key], $this->types[$key]);
             }
 
             $stopwatchEvent->stop();
