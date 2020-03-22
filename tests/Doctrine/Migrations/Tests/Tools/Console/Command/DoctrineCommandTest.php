662bdfd68478772494743cfb7afcbd965f7e20aa
Grégoire Paris <postmaster@greg0ire.fr>
2020-03-22 11:47:30 +0100

    Avoid mocking
    It just does not work, because the mock does not return an integer,
    which causes the test to fail with a TypeError

--- tests/Doctrine/Migrations/Tests/Tools/Console/Command/DoctrineCommandTest.php
+++ tests/Doctrine/Migrations/Tests/Tools/Console/Command/DoctrineCommandTest.php
@@ -7,6 +7,8 @@ namespace Doctrine\Migrations\Tests\Tools\Console\Command;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Tests\MigrationTestCase;
 use Doctrine\Migrations\Tools\Console\Command\DoctrineCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class DoctrineCommandTest extends MigrationTestCase
@@ -22,11 +24,13 @@ class DoctrineCommandTest extends MigrationTestCase
             ->expects(self::once())
             ->method('freeze');
 
-        $command = $this->getMockBuilder(DoctrineCommand::class)
-            ->setConstructorArgs([$dependencyFactory])
-            ->onlyMethods(['execute'])
-            ->getMockForAbstractClass();
-
+        $command       = new class($dependencyFactory) extends DoctrineCommand
+        {
+            protected function execute(InputInterface $input, OutputInterface $output) : int
+            {
+                return 0;
+            }
+        };
         $commandTester = new CommandTester($command);
 
         $commandTester->execute(
