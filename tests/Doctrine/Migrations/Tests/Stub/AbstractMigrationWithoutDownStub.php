47584209d71e7f0c1e30319e1605e342edfef98a
Asmir Mustafic <goetas@gmail.com>
2020-03-28 21:01:10 +0100

    Make the down() migration optional

---
+++ tests/Doctrine/Migrations/Tests/Stub/AbstractMigrationWithoutDownStub.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Migrations\Tests\Stub;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class AbstractMigrationWithoutDownStub extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+    }
+}
