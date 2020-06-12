4a236ba3c37c385af084ca220e97be7cac4b1a84
Grégoire Paris <postmaster@greg0ire.fr>
2020-06-12 21:28:29 +0200

    Address deprecations from doctrine/persistence
    Since it is only referenced in tests, the lib is adding as a dev
    requirement.
    
    Refs #983

--- tests/Doctrine/Migrations/Tests/Tools/Console/config/cli-config.php
+++ tests/Doctrine/Migrations/Tests/Tools/Console/config/cli-config.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Persistence\Mapping\Driver\PHPDriver;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\Mapping\Driver\PHPDriver;
 
 $conf = new Configuration();
 $conf->setProxyDir(__DIR__);
