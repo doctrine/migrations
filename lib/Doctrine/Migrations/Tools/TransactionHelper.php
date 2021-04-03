6d17f69e3f3029cf6aee90c566bea187906b9806
Grégoire Paris <postmaster@greg0ire.fr>
2021-04-03 17:04:54 +0200

    Rollback if and only if inside a transaction
    Similarly to what happens when attempting to commit an already closed
    connection when combining PHP 8 and pdo_mysql, an error is thrown when
    attempting to rollback. We use the same solution as for commit(), that
    is we check that we actually are inside a transaction when possible to do so.
    
    Fixes #1139

--- lib/Doctrine/Migrations/Tools/TransactionHelper.php
+++ lib/Doctrine/Migrations/Tools/TransactionHelper.php
@@ -14,13 +14,28 @@ final class TransactionHelper
 {
     public static function commitIfInTransaction(Connection $connection): void
     {
-        $wrappedConnection = $connection->getWrappedConnection();
-
-        // Attempt to commit while no transaction is running results in exception since PHP 8 + pdo_mysql combination
-        if ($wrappedConnection instanceof PDO && ! $wrappedConnection->inTransaction()) {
+        if (! self::inTransaction($connection)) {
             return;
         }
 
         $connection->commit();
+    }
+
+    public static function rollbackIfInTransaction(Connection $connection): void
+    {
+        if (! self::inTransaction($connection)) {
+            return;
+        }
+
+        $connection->rollBack();
+    }
+
+    private static function inTransaction(Connection $connection): bool
+    {
+        $wrappedConnection = $connection->getWrappedConnection();
+
+        /* Attempt to commit or rollback while no transaction is running
+           results in an exception since PHP 8 + pdo_mysql combination */
+        return ! $wrappedConnection instanceof PDO || $wrappedConnection->inTransaction();
     }
 }
