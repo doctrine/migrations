5b1325c50b6eafc319c86dfcdf2d1e842db9cedd
Grégoire Paris <postmaster@greg0ire.fr>
2021-04-11 23:39:17 +0200

    Call static method statically

--- tests/Doctrine/Migrations/Tests/Generator/ClassNameGeneratorTest.php
+++ tests/Doctrine/Migrations/Tests/Generator/ClassNameGeneratorTest.php
@@ -17,7 +17,7 @@ class ClassNameGeneratorTest extends TestCase
         $fqcn      = $generator->generateClassName('Foo');
 
         if (method_exists($this, 'assertMatchesRegularExpression')) {
-            $this->assertMatchesRegularExpression('/^Foo\\\\Version[0-9]{14}$/', $fqcn);
+            self::assertMatchesRegularExpression('/^Foo\\\\Version[0-9]{14}$/', $fqcn);
         } else {
             self::assertRegExp('/^Foo\\\\Version[0-9]{14}$/', $fqcn);
         }
