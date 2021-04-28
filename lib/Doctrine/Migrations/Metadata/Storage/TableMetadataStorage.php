b4892f69e87f4e70948758aac0bf6bab4c5c36e9
Frank Verhoeven <frankverhoeven@me.com>
2021-04-28 14:03:33 +0200

    Do not pass connection name to `PrimaryReadReplicaConnection::connect()`

--- lib/Doctrine/Migrations/Metadata/Storage/TableMetadataStorage.php
+++ lib/Doctrine/Migrations/Metadata/Storage/TableMetadataStorage.php
@@ -198,7 +198,7 @@ final class TableMetadataStorage implements MetadataStorage
         }
 
         if ($this->connection instanceof PrimaryReadReplicaConnection) {
-            $this->connection->connect('master');
+            $this->connection->connect();
         }
 
         return $this->schemaManager->tablesExist([$this->configuration->getTableName()]);
