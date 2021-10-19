doctrine/migrations

commit ef842cfb1944606148159e724d8ae7e7845cf4b0

Automatically fix cs

lib/Doctrine/Migrations/Tools/Console/Command/AbstractCommand.php
@@ -17,6 +17,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+
 use function assert;
 use function escapeshellarg;
 use function proc_open;
@@ -43,24 +44,24 @@ abstract class AbstractCommand extends Command
     /** @var Configuration|null */
     protected $migrationConfiguration;
 
-    public function setMigrationConfiguration(Configuration $configuration) : void
+    public function setMigrationConfiguration(Configuration $configuration): void
     {
         $this->configuration = $configuration;
 
         $this->initializeDependencies();
     }
 
-    public function setConnection(Connection $connection) : void
+    public function setConnection(Connection $connection): void
     {
         $this->connection = $connection;
     }
 
-    public function setDependencyFactory(DependencyFactory $dependencyFactory) : void
+    public function setDependencyFactory(DependencyFactory $dependencyFactory): void
     {
         $this->dependencyFactory = $dependencyFactory;
     }
 
-    public function setMigrationRepository(MigrationRepository $migrationRepository) : void
+    public function setMigrationRepository(MigrationRepository $migrationRepository): void
     {
         $this->migrationRepository = $migrationRepository;
     }
@@ -68,7 +69,7 @@ abstract class AbstractCommand extends Command
     public function initialize(
         InputInterface $input,
         OutputInterface $output
-    ) : void {
+    ): void {
         $this->configuration = $this->getMigrationConfiguration($input, $output);
 
         $this->initializeDependencies();
@@ -77,7 +78,7 @@ abstract class AbstractCommand extends Command
         $this->configuration->createMigrationTable();
     }
 
-    protected function configure() : void
+    protected function configure(): void
     {
         $this->addOption(
             'configuration',
@@ -96,7 +97,7 @@ abstract class AbstractCommand extends Command
 
     protected function outputHeader(
         OutputInterface $output
-    ) : void {
+    ): void {
         $name = $this->configuration->getName();
         $name = $name ?? 'Doctrine Database Migrations';
         $name = str_repeat(' ', 20) . $name . str_repeat(' ', 20);
@@ -109,13 +110,13 @@ abstract class AbstractCommand extends Command
     protected function getMigrationConfiguration(
         InputInterface $input,
         OutputInterface $output
-    ) : Configuration {
+    ): Configuration {
         if ($this->migrationConfiguration === null) {
             if ($this->hasConfigurationHelper()) {
                 $helperSet = $this->getHelperSet();
                 assert($helperSet !== null);
-                /** @var ConfigurationHelper $configHelper */
                 $configHelper = $helperSet->get('configuration');
+                assert($configHelper instanceof ConfigurationHelper);
             } else {
                 $configHelper = new ConfigurationHelper(
                     $this->getConnection($input),
@@ -126,7 +127,7 @@ abstract class AbstractCommand extends Command
             $this->migrationConfiguration = $configHelper->getMigrationConfig($input);
 
             $this->migrationConfiguration->getOutputWriter()->setCallback(
-                static function (string $message) use ($output) : void {
+                static function (string $message) use ($output): void {
                     $output->writeln($message);
                 }
             );
@@ -143,7 +144,7 @@ abstract class AbstractCommand extends Command
         string $question,
         InputInterface $input,
         OutputInterface $output
-    ) : bool {
+    ): bool {
         return $this->getHelper('question')->ask(
             $input,
             $output,
@@ -155,26 +156,26 @@ abstract class AbstractCommand extends Command
         string $question,
         InputInterface $input,
         OutputInterface $output
-    ) : bool {
+    ): bool {
         return ! $input->isInteractive() || $this->askConfirmation($question, $input, $output);
     }
 
-    protected function procOpen(string $editorCommand, string $path) : void
+    protected function procOpen(string $editorCommand, string $path): void
     {
         proc_open($editorCommand . ' ' . escapeshellarg($path), [], $pipes);
     }
 
-    private function initializeDependencies() : void
+    private function initializeDependencies(): void
     {
         $this->connection          = $this->configuration->getConnection();
         $this->dependencyFactory   = $this->configuration->getDependencyFactory();
         $this->migrationRepository = $this->dependencyFactory->getMigrationRepository();
     }
 
-    private function hasConfigurationHelper() : bool
+    private function hasConfigurationHelper(): bool
     {
-        /** @var HelperSet|null $helperSet */
         $helperSet = $this->getHelperSet();
+        assert($helperSet instanceof HelperSet || $helperSet === null);
 
         if ($helperSet === null) {
             return false;
@@ -187,7 +188,7 @@ abstract class AbstractCommand extends Command
         return $helperSet->get('configuration') instanceof ConfigurationHelperInterface;
     }
 
-    private function getConnection(InputInterface $input) : Connection
+    private function getConnection(InputInterface $input): Connection
     {
         if ($this->connection === null) {
             $helperSet = $this->getHelperSet();

commit 7f45928c36aca4058df0ef5a095dc05cc6373f16

Fix assert for configuration helper in AbstractCommand, assert interface instead of internal implementation.
Regression from ef842cfb1944606148159e724d8ae7e7845cf4b0

Fixes #1115

lib/Doctrine/Migrations/Tools/Console/Command/AbstractCommand.php
@@ -116,7 +116,7 @@ abstract class AbstractCommand extends Command
                 $helperSet = $this->getHelperSet();
                 assert($helperSet !== null);
                 $configHelper = $helperSet->get('configuration');
-                assert($configHelper instanceof ConfigurationHelper);
+                assert($configHelper instanceof ConfigurationHelperInterface);
             } else {
                 $configHelper = new ConfigurationHelper(
                     $this->getConnection($input),