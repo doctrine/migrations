7fa9d14fdee68edac7e2f1109d8c7c66a1414a34
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-12-09 12:22:12 +0100

    Leverage LazyProxyTrait in LazySchemaDiffProvider (#1273)

---
+++ lib/Doctrine/Migrations/Provider/LazySchema.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Migrations\Provider;
+
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\VarExporter\LazyProxyTrait;
+
+/**
+ * @internal
+ */
+class LazySchema extends Schema
+{
+    use LazyProxyTrait;
+}
--- lib/Doctrine/Migrations/Provider/LazySchemaDiffProvider.php
+++ lib/Doctrine/Migrations/Provider/LazySchemaDiffProvider.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 namespace Doctrine\Migrations\Provider;
 
 use Doctrine\DBAL\Schema\Schema;
-use ProxyManager\Configuration;
-use ProxyManager\Factory\LazyLoadingValueHolderFactory;
-use ProxyManager\GeneratorStrategy\EvaluatingGeneratorStrategy;
-use ProxyManager\Proxy\LazyLoadingInterface;
 
 /**
  * The LazySchemaDiffProvider is responsible for lazily generating the from schema when diffing two schemas
@@ -18,59 +14,27 @@ use ProxyManager\Proxy\LazyLoadingInterface;
  */
 class LazySchemaDiffProvider implements SchemaDiffProvider
 {
-    private LazyLoadingValueHolderFactory $proxyFactory;
-
     private SchemaDiffProvider $originalSchemaManipulator;
 
     public function __construct(
-        LazyLoadingValueHolderFactory $proxyFactory,
         SchemaDiffProvider $originalSchemaManipulator
     ) {
-        $this->proxyFactory              = $proxyFactory;
         $this->originalSchemaManipulator = $originalSchemaManipulator;
-    }
-
-    public static function fromDefaultProxyFactoryConfiguration(
-        SchemaDiffProvider $originalSchemaManipulator
-    ): LazySchemaDiffProvider {
-        $proxyConfig = new Configuration();
-        $proxyConfig->setGeneratorStrategy(new EvaluatingGeneratorStrategy());
-        $proxyFactory = new LazyLoadingValueHolderFactory($proxyConfig);
-
-        return new LazySchemaDiffProvider($proxyFactory, $originalSchemaManipulator);
     }
 
     public function createFromSchema(): Schema
     {
         $originalSchemaManipulator = $this->originalSchemaManipulator;
 
-        return $this->proxyFactory->createProxy(
-            Schema::class,
-            static function (&$wrappedObject, $proxy, $method, array $parameters, &$initializer) use ($originalSchemaManipulator): bool {
-                $initializer = null;
-
-                $wrappedObject = $originalSchemaManipulator->createFromSchema();
-
-                return true;
-            }
-        );
+        return LazySchema::createLazyProxy(static fn () => $originalSchemaManipulator->createFromSchema());
     }
 
     public function createToSchema(Schema $fromSchema): Schema
     {
         $originalSchemaManipulator = $this->originalSchemaManipulator;
 
-        if ($fromSchema instanceof LazyLoadingInterface && ! $fromSchema->isProxyInitialized()) {
-            return $this->proxyFactory->createProxy(
-                Schema::class,
-                static function (&$wrappedObject, $proxy, $method, array $parameters, &$initializer) use ($originalSchemaManipulator, $fromSchema): bool {
-                    $initializer = null;
-
-                    $wrappedObject = $originalSchemaManipulator->createToSchema($fromSchema);
-
-                    return true;
-                }
-            );
+        if ($fromSchema instanceof LazySchema && ! $fromSchema->isLazyObjectInitialized()) {
+            return LazySchema::createLazyProxy(static fn () => $originalSchemaManipulator->createToSchema($fromSchema));
         }
 
         return $this->originalSchemaManipulator->createToSchema($fromSchema);
@@ -80,8 +44,8 @@ class LazySchemaDiffProvider implements SchemaDiffProvider
     public function getSqlDiffToMigrate(Schema $fromSchema, Schema $toSchema): array
     {
         if (
-            $toSchema instanceof LazyLoadingInterface
-            && ! $toSchema->isProxyInitialized()
+            $toSchema instanceof LazySchema
+            && ! $toSchema->isLazyObjectInitialized()
         ) {
             return [];
         }
