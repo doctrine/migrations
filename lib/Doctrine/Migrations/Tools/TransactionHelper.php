ca8cb14a7d99ec2b304fad382a5ccaae2fa43a91
Grégoire Paris <gregoire.paris@manomano.com>
2023-01-11 10:04:11 +0100

    Drop support for silencing errors about DDL in transactions
    I hesitated between this and removing the transaction helper entirely,
    but I think this solution will result in less support for us.

--- lib/Doctrine/Migrations/Tools/TransactionHelper.php
+++ lib/Doctrine/Migrations/Tools/TransactionHelper.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Doctrine\Migrations\Tools;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\Deprecations\Deprecation;
 use LogicException;
 use PDO;
 
@@ -16,43 +15,35 @@ use function method_exists;
  */
 final class TransactionHelper
 {
-    public static function commitIfInTransaction(Connection $connection): void
+    public static function commit(Connection $connection): void
     {
         if (! self::inTransaction($connection)) {
-            Deprecation::trigger(
-                'doctrine/migrations',
-                'https://github.com/doctrine/migrations/issues/1169',
-                <<<'DEPRECATION'
+            throw new LogicException(
+                <<<'EXCEPTION'
 Context: trying to commit a transaction
-Problem: the transaction is already committed, relying on silencing is deprecated.
+Problem: the transaction is already committed.
 Solution: override `AbstractMigration::isTransactional()` so that it returns false.
 Automate that by setting `transactional` to false in the configuration.
 More details at https://www.doctrine-project.org/projects/doctrine-migrations/en/3.2/explanation/implicit-commits.html
-DEPRECATION
+EXCEPTION
             );
-
-            return;
         }
 
         $connection->commit();
     }
 
-    public static function rollbackIfInTransaction(Connection $connection): void
+    public static function rollback(Connection $connection): void
     {
         if (! self::inTransaction($connection)) {
-            Deprecation::trigger(
-                'doctrine/migrations',
-                'https://github.com/doctrine/migrations/issues/1169',
-                <<<'DEPRECATION'
+            throw new LogicException(
+                <<<'EXCEPTION'
 Context: trying to rollback a transaction
-Problem: the transaction is already rolled back, relying on silencing is deprecated.
+Problem: the transaction is already rolled back.
 Solution: override `AbstractMigration::isTransactional()` so that it returns false.
 Automate that by setting `transactional` to false in the configuration.
 More details at https://www.doctrine-project.org/projects/doctrine-migrations/en/3.2/explanation/implicit-commits.html
-DEPRECATION
+EXCEPTION
             );
-
-            return;
         }
 
         $connection->rollBack();
