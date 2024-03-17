2b3af05828f4773de9c7efea01cdb3ad5a632bac
Willem Mouwen <willem@mouwen.dev>
2024-03-17 19:29:27 +0100

    Add test case

--- lib/Doctrine/Migrations/Event/Listeners/RemoveMigrationTableFromSchemaListener.php
+++ lib/Doctrine/Migrations/Event/Listeners/RemoveMigrationTableFromSchemaListener.php
@@ -5,24 +5,19 @@ declare(strict_types=1);
 namespace Doctrine\Migrations\Event\Listeners;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Configuration\Configuration;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
 
 class RemoveMigrationTableFromSchemaListener
 {
-    private TableMetadataStorageConfiguration|null $configuration = null;
+    private Configuration $configuration;
 
     public function __construct(
-        private readonly DependencyFactory $dependencyFactory,
+        DependencyFactory $dependencyFactory,
     ) {
-        $configuration = $this->dependencyFactory->getConfiguration()->getMetadataStorageConfiguration();
-
-        if (! ($configuration instanceof TableMetadataStorageConfiguration)) {
-            return;
-        }
-
-        $this->configuration = $configuration;
+        $this->configuration = $dependencyFactory->getConfiguration();
     }
 
     public function postGenerateSchema(GenerateSchemaEventArgs $args): void
@@ -37,11 +32,13 @@ class RemoveMigrationTableFromSchemaListener
 
     private function removeMigrationsTableFromSchema(Schema $schema): void
     {
-        if (! ($this->configuration instanceof TableMetadataStorageConfiguration)) {
+        $metadataConfiguration = $this->configuration->getMetadataStorageConfiguration();
+
+        if (! ($metadataConfiguration instanceof TableMetadataStorageConfiguration)) {
             return;
         }
 
-        $tableName = $this->configuration->getTableName();
+        $tableName = $metadataConfiguration->getTableName();
 
         if (! $schema->hasTable($tableName)) {
             return;
---
+++ tests/Doctrine/Migrations/Tests/Event/Listeners/RemoveMigrationTableFromSchemaListenerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Migrations\Tests\Event\Listeners;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Configuration\Configuration;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\ExistingConfiguration;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Event\Listeners\RemoveMigrationTableFromSchemaListener;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
+use Doctrine\Migrations\Tests\MigrationTestCase;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+class RemoveMigrationTableFromSchemaListenerTest extends MigrationTestCase
+{
+    private RemoveMigrationTableFromSchemaListener $listener;
+    private Configuration $configuration;
+    private EntityManagerInterface $entityManager;
+
+    public function testListenerRemovesMigrationSchema(): void
+    {
+        $metadataConfiguration = new TableMetadataStorageConfiguration();
+        $tableName             = $metadataConfiguration->getTableName();
+        $this->configuration->setMetadataStorageConfiguration($metadataConfiguration);
+
+        $schema = new Schema();
+        $schema->createTable($tableName);
+
+        static::assertTrue($schema->hasTable($tableName));
+
+        $this->listener->postGenerateSchema(new GenerateSchemaEventArgs($this->entityManager, $schema));
+
+        static::assertFalse($schema->hasTable($tableName));
+    }
+
+    public function testListenerIgnoresMissingTable(): void
+    {
+        $metadataConfiguration = new TableMetadataStorageConfiguration();
+        $tableName             = $metadataConfiguration->getTableName();
+        $this->configuration->setMetadataStorageConfiguration($metadataConfiguration);
+
+        $schema = new Schema();
+
+        static::assertFalse($schema->hasTable($tableName));
+
+        $this->listener->postGenerateSchema(new GenerateSchemaEventArgs($this->entityManager, $schema));
+
+        static::assertFalse($schema->hasTable($tableName));
+    }
+
+    public function testListenerIgnoresMissingConfiguration(): void
+    {
+        $this->listener->postGenerateSchema(new GenerateSchemaEventArgs($this->entityManager, new Schema()));
+
+        static::assertTrue(true);
+    }
+
+    protected function setUp(): void
+    {
+        $this->entityManager = $this->createMock(EntityManager::class);
+
+        $this->configuration = new Configuration();
+
+        $dependencyFactory = DependencyFactory::fromEntityManager(
+            new ExistingConfiguration($this->configuration),
+            new ExistingEntityManager($this->entityManager),
+        );
+
+        $this->listener = new RemoveMigrationTableFromSchemaListener($dependencyFactory);
+    }
+}
